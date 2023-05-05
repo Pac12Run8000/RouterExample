441acb25c0ab036dfb43ee05ebfbd75d83fdf950
Pac12Run8000 <gangstarr78992@>
2023-05-04 20:19:09 -0700

    Initial Commit

---
+++ RouterExample/RouterExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouterExampleApp.swift
+//  RouterExample
+//
+//  Created by Michelle Grover on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct RouterExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
